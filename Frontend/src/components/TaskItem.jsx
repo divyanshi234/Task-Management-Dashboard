13854e09545ff2725c062a6d4eb9437081dd7a4e
Divyanshi Rajput <168019864+divyanshi234@users.noreply.github.com>
2026-01-02 19:01:10 +0530

    Update TaskItem.jsx

--- Frontend/src/components/TaskItem.jsx
+++ Frontend/src/components/TaskItem.jsx
@@ -7,7 +7,7 @@ import axios from 'axios';
 import TaskModal from './TaskModal';
 
 
-const API_BASE = "http://localhost:4000/api/tasks";
+const API_BASE = `${import.meta.env.VITE_API_URL}/api/tasks`;
 
 
 const TaskItem = ({task,onRefresh, onLogout, showCompletedCheckbox = true}) => {
